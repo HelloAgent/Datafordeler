506f7fb17d241816fe866baff4cc0fceb7702705
Simon Saxtorph <simon@helloagent.dk>
2021-06-30 17:33:09 +0200

    docs: added schema files from datafordeler

--- src/services/ejf/models/ejerskifte.ts
+++ src/services/ejf/models/ejerskifte.ts
@@ -36,6 +36,7 @@ export interface Ejerskifte {
     anmeldelsesdato: string;
     overdragelsesmaade: string;
     bestemtFastEjendomBFENr: number;
+    ejerskabsskifte?: string[];
     handelsoplysningerLokalId: string;
   };
   type: "Feature";
