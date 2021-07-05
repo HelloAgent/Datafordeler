5fa108d9dcbefd616725e3fccfcee7dd02d50663
Simon Saxtorph <simon@helloagent.dk>
2021-07-05 15:22:24 +0200

    fix: request types

--- src/services/bbr/models/bygning.ts
+++ src/services/bbr/models/bygning.ts
@@ -10,26 +10,49 @@ import { BygningPaaFremmedGrundList } from "./bygningPaaFremmedGrund";
  * Obligatoriske parametre:
   Et ID på enten Bygning, Grund, Enhed, TekniskAnlæg, BBRSag eller Ejendomsrelation og et interval for enten registreringstid, virkningstid eller DAF opdateringstid.
  */
+
+export type Status =
+  | "1"
+  | "2"
+  | "3"
+  | "4"
+  | "5"
+  | "6"
+  | "7"
+  | "8"
+  | "9"
+  | "10"
+  | "11"
+  | "12"
+  | "13"
+  | "14"
+  | "15"
+  | "16"
+  | "17"
+  | "18"
+  | "19";
+
 export interface BygningRequest {
-  Id?: string; //can be array!
+  Id?: string | string[]; //can be array!
   VirkningFra?: Date;
   VirkningTil?: Date;
   Virkningsaktoer?: string;
   RegistreringFra?: Date;
   RegistreringTil?: Date;
   Registreringsaktoer?: string;
-  Status?: string; //can be array of 1-19, see https://teknik.bbr.dk/kodelister/0/1/0/Livscyklus
+  Status?: Status | Status[]; //can be array of 1-19, see https://teknik.bbr.dk/kodelister/0/1/0/Livscyklus
   Forretningsproces?: string;
   Forretningsomraade?: string;
   Forretningshaendelse?: string;
+  Husnummer?: string; //Dar HusnummerId
   Kommunekode?: string;
   MedDybde?: boolean;
   DAFTimestampFra?: Date;
   DAFTimestampTil?: Date;
-  Etage?: string; //BBR etage ID, can be array of UUID
-  Fordelingsareal?: string; //BBR fordelingsareal UUID
-  Opgang?: string; //BBR opgang UUIDcan be array of UUID
-  TekniskAnlaeg?: string; //BBR teknisk anlæg Id UUID
+  Etage?: string | string[]; //BBR etage ID, can be array of UUID
+  Fordelingsareal?: string | string[]; //BBR fordelingsareal UUID
+  Opgang?: string | string[]; //BBR opgang UUIDcan be array of UUID
+  TekniskAnlaeg?: string | string[]; //BBR teknisk anlæg Id UUID
   Grund?: string; //BBR Grund UUID
   Jordstykke?: number; //MU Jordstykke ID
   Ejendomsrelation?: string; //BBR ejd.relation ID
