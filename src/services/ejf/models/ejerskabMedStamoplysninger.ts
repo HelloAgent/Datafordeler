a4b6754783cc033f17308d77712f2588689398be
Simon Saxtorph <simon@helloagent.dk>
2021-12-20 15:17:01 +0100

    fix: updated types for OwnerCompany

--- src/services/ejf/models/ejerskabMedStamoplysninger.ts
+++ src/services/ejf/models/ejerskabMedStamoplysninger.ts
@@ -44,23 +44,31 @@ export declare namespace EjerskabMedStamoplysninger {
   export interface EjendeVirksomhed {
     entityType: string;
     attributes: {
-      registreringFra: string;
-      registreringsaktoer: string;
-      status: string;
-      virkningFra: string;
-      virkningsaktoer: string;
-      CVRNummer: number;
-      reklamebeskyttelse: boolean;
-      navn: string;
-      beliggenhedsadresse: {
-        "CVRAdresse.vejkode": string;
-        "CVRAdresse.husnummerFra": string;
-        "CVRAdresse.kommunekode": string;
-        "CVRAdresse.kommunenavn": string;
-        "CVRAdresse.postdistrikt": string;
-        "CVRAdresse.vejnavn": string;
-        "CVRAdresse.postnummer": string;
-        "CVRAdresse.landekode": string;
+      registreringFra?: string;
+      registreringTil?: string;
+      registreringsaktoer?: string;
+      status?: string;
+      virkningFra?: string;
+      virkningTil?: string;
+      virkningsaktoer?: string;
+      CVRNummer?: number;
+      reklamebeskyttelse?: boolean;
+      navn?: string;
+      beliggenhedsadresse?: {
+        "CVRAdresse.adresseFritekst"?: string;
+        "CVRAdresse.doerbetegnelse"?: string;
+        "CVRAdresse.etagebetegnelse"?: string;
+        "CVRAdresse.husnummerFra"?: string;
+        "CVRAdresse.husnummerTil"?: string;
+        "CVRAdresse.kommunekode"?: string;
+        "CVRAdresse.kommunenavn"?: string;
+        "CVRAdresse.landekode"?: string;
+        "CVRAdresse.postdistrikt"?: string;
+        "CVRAdresse.postnummer"?: string;
+        "CVRAdresse.supplerendeBynavn"?: string;
+        "CVRAdresse.vejkode"?: string;
+        "CVRAdresse.vejnavn"?: string;
+        coNavn?: string;
       };
     };
   }
