2e5819924488d86f040695acb185d9904e0d4f06
Simon Saxtorph <simon@saxs.dk>
2024-03-21 23:43:45 +0100

    chore: main structure

---
+++ packages/core/jest.config.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { unit } from '@datafordeler/config/jest/jest.config';
+
+export default async () => unit({ displayName: 'Core [Unit tests]' });
