eeaff8457985fd3a749f88c85c045884d25c818b
Simon Saxtorph <simon@saxs.dk>
2024-03-25 19:39:12 +0100

    Add types for client request parameters

---
+++ packages/core/src/types.ts
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-duplicate-enum-values */
+
+/**
+ * Endpoints for the client to call.
+ */
+export enum Endpoint {
+  PUBLIC = 'services.datafordeler.dk',
+  PUBLIC_PROTECTED = 'services.datafordeler.dk',
+  CERT0 = 'certservices.datafordeler.dk',
+  CERT5 = 's5-certservices.datafordeler.dk',
+}
+
+/**
+ * Values that can be passed as parameters to the request.
+ */
+export type ParameterValues =
+  | string
+  | number
+  | boolean
+  | Array<string | number>;
+
+/**
+ * Parameters that can be passed to the request.
+ */
+export type Parameters<T> = {
+  [key in keyof T]: ParameterValues;
+};
+
+/**
+ * Request parameters for the client to call.
+ *
+ * The url structure is as follows:
+ *
+ * `endpoint/register/service/version/servicetype/method?param1=value1&param2=value2`
+ *
+ * register and serviceType are static properties of the client, and such only the endpoint, service and method is required.
+ *
+ * The version is set by the client as well, but can be overwriten in the request.
+ */
+export interface RequestParams<T> {
+  /**
+   * The endpoint to call
+   * @example Endpoint.PUBLIC
+   */
+  endpoint: Endpoint;
+
+  /**
+   * The service to call
+   * @example 'DAR_BFE_Public'
+   */
+  service: string;
+
+  /**
+   * Override the version applied in the client
+   * @example '1'
+   */
+  version?: string;
+
+  /**
+   * The method to call
+   * @example 'adresseTilEnhedBfe'
+   */
+  method: string;
+
+  /**
+   * The parameters to pass to the method
+   */
+  params: Parameters<T>;
+}
+
