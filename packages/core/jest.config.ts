0d839e577e7d37683963d803f4876197a634eb12
Simon Saxtorph <simon@saxs.dk>
2024-03-25 20:29:43 +0100

    Add setupFiles to jest.config.ts

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -1,4 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { unit } from '@datafordeler/config/jest/jest.config';
 
-export default async () => unit({ displayName: 'Core [Unit tests]' });
+export default async () =>
+  unit({
+    displayName: 'Core [Unit tests]',
+    setupFiles: ['<rootDir>/jest.polyfills.js'],
+  });
