3f2e4ecf94a6d59d8e8e2538e0b9e55dd058e4fe
Simon Saxtorph <simon@saxs.dk>
2024-08-20 14:16:16 +0200

    feat: refactored config and tests

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,3 +1,4 @@
+/* Basic types */
 export type IdLokalId = string;
 export type IdNamespace = string;
 export type Status<T extends string = string> = T;
@@ -12,10 +13,20 @@ export type VirkningFra = DateString;
 export type VirkningTil = DateString | null;
 export type Virkningsaktoer = string;
 
-export type ForretningsHaendelse<T extends string = string> = T;
-export type ForretningsOmraade<T extends string = string> = T;
-export type ForretningsProces<T extends string = string> = T;
+export type Forretningshaendelse<T extends string = string> = T;
+export type Forretningsomraade<T extends string = string> = T;
+export type Forretningsproces<T extends string = string> = T;
 
+/* Types used in request objects */
+export type Registreringstid = DateString;
+export type RegistreringstidFra = DateString;
+export type RegistreringstidTil = DateString;
+
+export type Virkningstid = DateString;
+export type VirkningstidFra = DateString;
+export type VirkningstidTil = DateString;
+
+/* Types used in response objects */
 export interface Feature<T = Record<string, unknown>> {
   type: 'Feature';
   properties: T;
@@ -26,71 +37,71 @@ export interface FeatureCollection<T = Record<string, unknown>> {
   features: Feature<T>[];
 }
 
-export interface DatafordelerItem {
-  /**
-   * Lokal identifikation
-   */
-  id_lokalId: IdLokalId;
+// export interface DatafordelerItem {
+//   /**
+//    * Lokal identifikation
+//    */
+//   id_lokalId: IdLokalId;
 
-  /**
-   * Identifikation af namespace
-   * @example "http://data.gov.dk/ejerfortegnelse"
-   */
-  id_namespace: IdNamespace;
+//   /**
+//    * Identifikation af namespace
+//    * @example "http://data.gov.dk/ejerfortegnelse"
+//    */
+//   id_namespace: IdNamespace;
 
-  /**
-   * Forretningshændelse
-   * @example "Endeligt skøde"
-   */
-  forretningshaendelse: ForretningsHaendelse;
+//   /**
+//    * Forretningshændelse
+//    * @example "Endeligt skøde"
+//    */
+//   forretningshaendelse: Forretningshaendelse;
 
-  /**
-   * Forretningsområde
-   * @example "54.15"
-   */
-  forretningsomraade: ForretningsOmraade;
+//   /**
+//    * Forretningsområde
+//    * @example "54.15"
+//    */
+//   forretningsomraade: Forretningsomraade;
 
-  /**
-   * Forretningsproces
-   * @example "Hændelse fra Digital Tinglysning"
-   */
-  forretningsproces: ForretningsProces;
+//   /**
+//    * Forretningsproces
+//    * @example "Hændelse fra Digital Tinglysning"
+//    */
+//   forretningsproces: Forretningsproces;
 
-  /**
-   * Tidspunktet for objektets registrering
-   */
-  registreringFra: RegistreringFra;
+//   /**
+//    * Tidspunktet for objektets registrering
+//    */
+//   registreringFra: RegistreringFra;
 
-  /**
-   * Registreringsaktøer
-   * @example "ERST"
-   */
-  registreringsaktoer: Registreringsaktoer;
+//   /**
+//    * Registreringsaktøer
+//    * @example "ERST"
+//    */
+//   registreringsaktoer: Registreringsaktoer;
 
-  /**
-   * Tidspunktet hvor en ny registrering er foretaget på objektet, og hvor denne version ikke længere er den seneste
-   */
-  registreringTil?: RegistreringTil;
+//   /**
+//    * Tidspunktet hvor en ny registrering er foretaget på objektet, og hvor denne version ikke længere er den seneste
+//    */
+//   registreringTil?: RegistreringTil;
 
-  /**
-   * Tidspunktet hvor objektets virkning starter
-   */
-  virkningFra: VirkningFra;
+//   /**
+//    * Tidspunktet hvor objektets virkning starter
+//    */
+//   virkningFra: VirkningFra;
 
-  /**
-   * Tidspunktet hvor objektets virkning ophører
-   */
-  virkningTil?: VirkningTil;
+//   /**
+//    * Tidspunktet hvor objektets virkning ophører
+//    */
+//   virkningTil?: VirkningTil;
 
-  /**
-   * Virkningsaktøer
-   * @example "ERST"
-   */
-  virkningsaktoer: Virkningsaktoer;
+//   /**
+//    * Virkningsaktøer
+//    * @example "ERST"
+//    */
+//   virkningsaktoer: Virkningsaktoer;
 
-  /**
-   * Objektets status
-   * @example "gældende"
-   */
-  status: Status;
-}
+//   /**
+//    * Objektets status
+//    * @example "gældende"
+//    */
+//   status: Status;
+// }
