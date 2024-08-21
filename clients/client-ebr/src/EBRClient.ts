5df47dfa803c0703f5c05c28752311854ac2831b
Simon Saxtorph <simon@saxs.dk>
2024-08-21 22:23:30 +0200

    feat: added method for bfenradresse

--- clients/client-ebr/src/EBRClient.ts
+++ clients/client-ebr/src/EBRClient.ts
@@ -6,12 +6,53 @@ import {
   RequestBuilder,
 } from '@datafordeler/core';
 
+import type { BFEnrAdresseRequest, BFEnrAdresseResponse } from './types';
+
 export type EBRClientConfig = ClientBaseConfig & AuthConfig;
 
 export class EBR extends Client<EBRClientConfig> {
+  private hasAgent: boolean = false;
+
+  private hasCredentials: boolean = false;
+
   constructor(props: EBRClientConfig) {
     super('Ejendomsbeliggenhed', props);
+
+    if (props.agent || props.agentConfig) {
+      this.hasAgent = true;
+    }
+
+    if (props.credentials) {
+      this.hasCredentials = true;
+    }
   }
 
-  // Requests can be made with both credentials and with a certificate
+  /**
+   * BFEnrAdresse method using version 1
+   *
+   * Request can be made with either an agent or credentials. If both are provided, agent will be used.
+   */
+  async BFEnrAdresse(params: BFEnrAdresseRequest) {
+    const request = new RequestBuilder<
+      BFEnrAdresseRequest,
+      BFEnrAdresseResponse
+    >()
+      .setService('Ejendomsbeliggenhed')
+      .setVersion('1')
+      .setServiceType('REST')
+      .setMethod('BFEnrAdresse')
+      .setParams(params);
+
+    if (this.hasAgent) {
+      request.setEndpoint(Endpoint.CERT0);
+    } else if (this.hasCredentials) {
+      request.setEndpoint(Endpoint.PUBLIC_PROTECTED);
+    } else {
+      throw new Error(
+        'No authentication configuration set. Please provide either agent/agent configuration or credentials.',
+      );
+    }
+
+    return this.request(request.build());
+  }
 }
