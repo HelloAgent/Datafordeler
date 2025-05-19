fef157b2f7785cc20a9554ad4f9f5c947b742f09
Simon Saxtorph <simon@saxs.dk>
2025-05-19 14:39:36 +0200

    Add generated TypeScript schemas for BBR 2.4.4 Grund and Teknisk Anlæg
    - Introduced BBR_244_Grund schema with various properties related to property registration and management.
    - Introduced BBR_244_TekniskAnlæg schema encompassing technical details of installations and structures.
    - Both schemas include types for update timestamps and various identifiers, ensuring type safety and clarity in data handling.

---
+++ clients/client-bbr/src/types/generated/BBR_2.4.4_BBRSag.schema.ts
@@ -0,0 +1,284 @@
+/**
+ * This file was automatically generated.
+ * DO NOT MODIFY IT BY HAND. Instead, modify the source JSONSchema file,
+ * and run 'pnpm generate:types' to regenerate this file.
+ */
+
+/**
+ * UpdateTimestamp()
+ */
+export type DatafordelerOpdateringstid = string;
+/**
+ * UpdateTimestamp()
+ */
+export type DatafordelerOpdateringstid1 = string;
+export type BBR_244_BBRSag = {
+  datafordelerOpdateringstid?: DatafordelerOpdateringstid;
+  /**
+   * EAID_50066AF4_06D9_4f3a_BAC2_5F83ABF0E335
+   */
+  forretningshændelse?: string | null;
+  /**
+   * EAID_C91552B7_18C0_4e28_8B06_F60332A8776B
+   */
+  forretningsområde?: string | null;
+  /**
+   * EAID_E4132152_7923_4533_8529_5E8D8C937656
+   */
+  forretningsproces?: string | null;
+  /**
+   * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_9AB90AE0_9F85_4164_9B25_8EB2139D65A5
+   */
+  id_lokalId: string | null;
+  /**
+   * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_6CEB0356_5CBF_4159_B96B_A2489DD2DAC8
+   */
+  id_namespace?: string | null;
+  /**
+   * EAID_C95C717A_752C_4b17_B647_850738975CAC
+   */
+  kommunekode?: string | null;
+  /**
+   * EAID_7A7F26D9_D5AE_4158_A7FF_3E6EF0B228D8
+   */
+  registreringFra?: string | null;
+  /**
+   * EAID_B3CCBA6B_52B2_4ddc_A751_ABEA39D6EE6B
+   */
+  registreringsaktør?: string | null;
+  /**
+   * EAID_E718B937_950A_4ea1_BFA0_2AE9DB8C43AD
+   */
+  registreringTil?: string | null;
+  /**
+   * EAID_E140451A_2036_45e2_90BD_4E5A0BF90D0F
+   */
+  sag001Byggesagsnummer?: string | null;
+  /**
+   * EAID_D8ED0D23_B832_4ad1_8F58_61960AD3ABE1
+   */
+  sag002Byggesagsdato?: string | null;
+  /**
+   * EAID_A7088332_3D4D_41ac_B605_2D19D7E323A2
+   */
+  sag003Byggetilladelsesdato?: string | null;
+  /**
+   * EAID_52477D8B_E38B_437e_BBC4_1CFC01AFB6FE
+   */
+  sag004ForventetPåbegyndelsesdato?: string | null;
+  /**
+   * EAID_CB256EE7_79C5_4a0e_99B7_B4D0791D1433
+   */
+  sag005Påbegyndelsesdato?: string | null;
+  /**
+   * EAID_D4341684_983F_46ed_9A80_5636E32E9225
+   */
+  sag006IbrugtagningsTilladelse?: string | null;
+  /**
+   * EAID_76DA68F2_FEBE_4722_80A9_2DDBD1F344AE
+   */
+  sag007Henlæggelse?: string | null;
+  /**
+   * EAID_314537D6_8C7E_4832_99AD_420B35201F94
+   */
+  sag008FærdigtBygningsareal?: number | null;
+  /**
+   * EAID_295DF59C_5C6E_41e2_B746_B4F51D2EFF26
+   */
+  sag009ForventetFuldførtDato?: string | null;
+  /**
+   * EAID_DCB9D69B_7898_4d92_9244_661E916420BF
+   */
+  sag010FuldførelseAfByggeri?: string | null;
+  /**
+   * EAID_AC6C18C4_0FD4_4c96_9A19_83EDA072297A
+   */
+  sag012Byggesagskode?: string | null;
+  /**
+   * EAID_B1DB1566_F2E8_4e4c_A40F_31BA0E9BDAD1
+   */
+  sag013AnmeldelseAfByggearbejde?: string | null;
+  /**
+   * EAID_23F221CD_5D00_49e7_B0BB_327521368087
+   */
+  sag016DelvisIbrugtagningsTilladelse?: string | null;
+  /**
+   * EAID_68E3D8A3_F1AD_4983_B98D_11F41EE40056
+   */
+  sag017ForeløbigFærdiggjortBygningsareal?: number | null;
+  /**
+   * EAID_7E080221_F45F_41fe_8F97_BCEF56D5F6F3
+   */
+  sag018ForeløbigFærdiggjortAntalLejligheder?: number | null;
+  /**
+   * EAID_A6E6C081_BB14_49fd_998E_EC5EDAE8F3ED
+   */
+  sag019Bygherreforhold?: string | null;
+  /**
+   * EAID_E58EC61F_9EE0_4372_B8BA_2C2320428FB8
+   */
+  sag024DatoForModtagelseAfAnsøgningOmByggetilladelse?: string | null;
+  /**
+   * EAID_8D2B8BFE_8624_4efc_A959_B44DD502E7AB
+   */
+  sag025DatoForFyldestgørendeAnsøgning?: string | null;
+  /**
+   * EAID_7FE5B566_4D94_4f8c_AD14_EDEF080526FE
+   */
+  sag026DatoForNaboorientering?: string | null;
+  /**
+   * EAID_89FC5530_E331_406b_9D4B_08196CD1A185
+   */
+  sag027DatoForFærdigbehandlingAfNaboorientering?: string | null;
+  /**
+   * EAID_884D4C3C_7D4B_435d_AEF6_4A0A2748BD13
+   */
+  sag032Litra?: string | null;
+  /**
+   * EAID_E93C5D1F_538F_4f9f_84EB_0DF7F9D877C2
+   */
+  sag033ForeløbigFærdiggjortAntalLejlighederUdenKøkken?: number | null;
+  /**
+   * EAID_6FAA8A1A_5BCF_4950_B220_F4AFCA7F35E3
+   */
+  status?: string | null;
+  /**
+   * EAID_A351A366_2F30_4c3f_B086_7FAA59339C5D
+   */
+  virkningFra?: string | null;
+  /**
+   * EAID_59A458B1_5844_4fe3_95B1_C38AD3AAFAF4
+   */
+  virkningsaktør?: string | null;
+  /**
+   * EAID_47628A51_6589_4f1e_A19F_E9943A136AEC
+   */
+  virkningTil?: string | null;
+  /**
+   * EAID_26A524FE_84B5_4ab2_B2E8_50E61410CCCA
+   */
+  sagsniveauList?: {
+    /**
+     * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_9AB90AE0_9F85_4164_9B25_8EB2139D65A5
+     */
+    id_lokalId: string | null;
+    /**
+     * EAID_26A524FE_84B5_4ab2_B2E8_50E61410CCCA
+     */
+    sagsniveau?: {
+      datafordelerOpdateringstid?: DatafordelerOpdateringstid1;
+      /**
+       * EAID_dst691B23_BDC2_457d_8AD0_4DC1D7573F28
+       */
+      byggesag?: string | null;
+      /**
+       * EAID_50066AF4_06D9_4f3a_BAC2_5F83ABF0E335
+       */
+      forretningshændelse?: string | null;
+      /**
+       * EAID_C91552B7_18C0_4e28_8B06_F60332A8776B
+       */
+      forretningsområde?: string | null;
+      /**
+       * EAID_E4132152_7923_4533_8529_5E8D8C937656
+       */
+      forretningsproces?: string | null;
+      /**
+       * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_9AB90AE0_9F85_4164_9B25_8EB2139D65A5
+       */
+      id_lokalId?: string | null;
+      /**
+       * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_6CEB0356_5CBF_4159_B96B_A2489DD2DAC8
+       */
+      id_namespace?: string | null;
+      /**
+       * EAID_C95C717A_752C_4b17_B647_850738975CAC
+       */
+      kommunekode?: string | null;
+      /**
+       * EAID_06BECD20_B0AF_4b97_8E33_A5A2A5FD0E06
+       */
+      niveautype?: string | null;
+      /**
+       * EAID_7A7F26D9_D5AE_4158_A7FF_3E6EF0B228D8
+       */
+      registreringFra?: string | null;
+      /**
+       * EAID_B3CCBA6B_52B2_4ddc_A751_ABEA39D6EE6B
+       */
+      registreringsaktør?: string | null;
+      /**
+       * EAID_E718B937_950A_4ea1_BFA0_2AE9DB8C43AD
+       */
+      registreringTil?: string | null;
+      /**
+       * EAID_dstC79896_F2B3_4191_ABFD_40ADB0F5D553
+       */
+      sagsdataBygning?: string | null;
+      /**
+       * EAID_dstE83EDB_37E2_444c_B8FD_DC3EFD442BF5
+       */
+      sagsdataEnhed?: string | null;
+      /**
+       * EAID_dst1BC869_614E_41c3_9B97_43BCD8640B7E
+       */
+      sagsdataEtage?: string | null;
+      /**
+       * EAID_dst8FCDB6_C960_43a6_93E3_9C5E6CD6EA95
+       */
+      sagsdataGrund?: string | null;
+      /**
+       * EAID_dst6B37D4_E395_49b7_93BE_ED19EE7228B2
+       */
+      sagsdataOpgang?: string | null;
+      /**
+       * EAID_dst4FBB79_2242_4424_8583_50ED2E237F11
+       */
+      sagsdataTekniskAnlæg?: string | null;
+      /**
+       * EAID_5A063470_2D13_4a5b_A56A_0073085FFC9B
+       */
+      sagstype?: string | null;
+      /**
+       * EAID_dstA6507B_AC9B_4292_BC5C_247F3AAF8E36
+       */
+      stamdataBygning?: string | null;
+      /**
+       * EAID_dst2C30E4_11C3_4d94_8336_B87025F0C28C
+       */
+      stamdataEnhed?: string | null;
+      /**
+       * EAID_dstDE669B_8642_4df9_B8E9_108EDA783EFE
+       */
+      stamdataEtage?: string | null;
+      /**
+       * EAID_dstD5F84B_B28D_436b_A95C_EF514341227C
+       */
+      stamdataGrund?: string | null;
+      /**
+       * EAID_dstE79DA4_1B42_4292_A54C_17FEB486558C
+       */
+      stamdataOpgang?: string | null;
+      /**
+       * EAID_dst6BB57E_DF69_4d6c_B5D6_F941B00933C7
+       */
+      stamdataTekniskAnlæg?: string | null;
+      /**
+       * EAID_6FAA8A1A_5BCF_4950_B220_F4AFCA7F35E3
+       */
+      status?: string | null;
+      /**
+       * EAID_A351A366_2F30_4c3f_B086_7FAA59339C5D
+       */
+      virkningFra?: string | null;
+      /**
+       * EAID_59A458B1_5844_4fe3_95B1_C38AD3AAFAF4
+       */
+      virkningsaktør?: string | null;
+      /**
+       * EAID_47628A51_6589_4f1e_A19F_E9943A136AEC
+       */
+      virkningTil?: string | null;
+    };
+  }[];
+}[];
---
+++ clients/client-bbr/src/types/generated/BBR_2.4.4_Enhed.schema.ts
@@ -0,0 +1,475 @@
+/**
+ * This file was automatically generated.
+ * DO NOT MODIFY IT BY HAND. Instead, modify the source JSONSchema file,
+ * and run 'pnpm generate:types' to regenerate this file.
+ */
+
+/**
+ * UpdateTimestamp()
+ */
+export type DatafordelerOpdateringstid = string;
+/**
+ * UpdateTimestamp()
+ */
+export type DatafordelerOpdateringstid1 = string;
+/**
+ * UpdateTimestamp()
+ */
+export type DatafordelerOpdateringstid2 = string;
+export type BBR_244_Enhed = {
+  datafordelerOpdateringstid?: DatafordelerOpdateringstid;
+  /**
+   * EAID_dst8AF9F7_45D9_485b_8E07_ABC985EE8E17
+   */
+  adresseIdentificerer?: string | null;
+  /**
+   * EAID_dst8522A1_2453_47e4_91D8_96C815F03DEB
+   */
+  bygning?: string | null;
+  /**
+   * EAID_587E34E4_7920_4b83_A1F4_DBDBEA2359FC
+   */
+  enh008UUIDTilModerlejlighed?: string | null;
+  /**
+   * EAID_4119CA74_2084_4297_8E0B_EBC04B94ABF9
+   */
+  enh020EnhedensAnvendelse?: string | null;
+  /**
+   * EAID_32317F84_52C9_4184_86D8_AEBC9FAED788
+   */
+  enh023Boligtype?: string | null;
+  /**
+   * EAID_D574CA64_2386_42f0_9B77_2DA13548F7F8
+   */
+  enh024KondemneretBoligenhed?: string | null;
+  /**
+   * EAID_37BECF34_168D_4c1e_B682_8CB74BF1C800
+   */
+  enh025OprettelsesdatoForEnhedensIdentifikation?: string | null;
+  /**
+   * EAID_389D57B2_479C_41d4_A79A_D9C7FA5D261F
+   */
+  enh026EnhedensSamledeAreal?: number | null;
+  /**
+   * EAID_5F5A8D13_13DA_41b0_99C5_BEDABC8DC547
+   */
+  enh027ArealTilBeboelse?: number | null;
+  /**
+   * EAID_4064EDFC_FE1E_4176_9964_42366C5C0742
+   */
+  enh028ArealTilErhverv?: number | null;
+  /**
+   * EAID_2EC3BF21_1383_46e7_8054_9825299A1231
+   */
+  enh030KildeTilEnhedensArealer?: string | null;
+  /**
+   * EAID_B4FB9125_9910_4adf_82AF_50985A30BA44
+   */
+  enh031AntalVærelser?: number | null;
+  /**
+   * EAID_DF448EE9_5C45_419c_B644_BDFEA42D3CDA
+   */
+  enh032Toiletforhold?: string | null;
+  /**
+   * EAID_9605CCCD_2206_4639_8A3C_5554A05829C8
+   */
+  enh033Badeforhold?: string | null;
+  /**
+   * EAID_183F63EC_6F82_43ad_BEA8_1646C7AC3862
+   */
+  enh034Køkkenforhold?: string | null;
+  /**
+   * EAID_7135CB8C_3954_42fe_88A6_46D4D95ED2C6
+   */
+  enh035Energiforsyning?: string | null;
+  /**
+   * EAID_0D1B57B3_A4AB_4ed9_94E4_757D780E3D06
+   */
+  enh039AndetAreal?: number | null;
+  /**
+   * EAID_3FF5B6B5_CC6E_49b3_AD30_D660A13954BD
+   */
+  enh041LovligAnvendelse?: string | null;
+  /**
+   * EAID_178C04BF_724F_4b66_B977_D5E31B51AFEF
+   */
+  enh042DatoForTidsbegrænsetDispensation?: string | null;
+  /**
+   * EAID_E40D83B6_CCA6_4592_A2FC_A370D36018F1
+   */
+  enh044DatoForDelvisIbrugtagningsTilladelse?: string | null;
+  /**
+   * EAID_411FDB4B_8DF2_430f_B476_397970713394
+   */
+  enh045Udlejningsforhold?: string | null;
+  /**
+   * EAID_CE10502D_09FD_4892_943E_517A14F318AD
+   */
+  enh046OffentligStøtte?: string | null;
+  /**
+   * EAID_33112690_D6E9_4171_A6AB_D3C895A726D2
+   */
+  enh047IndflytningDato?: string | null;
+  /**
+   * EAID_15A13E8D_3650_4d01_80EA_1FE52B63A2B5
+   */
+  enh048GodkendtTomBolig?: string | null;
+  /**
+   * EAID_48D98C4C_DB6A_4525_B8DE_9D4621CBF9D6
+   */
+  enh051Varmeinstallation?: string | null;
+  /**
+   * EAID_99C4D875_7CB4_4802_8CFD_A0132C4AD2E7
+   */
+  enh052Opvarmningsmiddel?: string | null;
+  /**
+   * EAID_569958AD_E09F_4c95_A67C_DE30093620B1
+   */
+  enh053SupplerendeVarme?: string | null;
+  /**
+   * EAID_89A64AD0_3A0D_4304_958C_F63B47880017
+   */
+  enh060EnhedensAndelFællesAdgangsareal?: number | null;
+  /**
+   * EAID_C170ABAB_F927_4573_8D69_02C36C7C35E0
+   */
+  enh061ArealAfÅbenOverdækning?: number | null;
+  /**
+   * EAID_EFE0BB75_4E81_48ce_9BAE_A14140B4CF51
+   */
+  enh062ArealAfLukketOverdækningUdestue?: number | null;
+  /**
+   * EAID_81F3C1A9_9C06_410c_ADDE_A2E7BCFEFDD9
+   */
+  enh063AntalVærelserTilErhverv?: number | null;
+  /**
+   * EAID_29D0597C_FB53_4e10_A7F3_1FE7EE8429F2
+   */
+  enh065AntalVandskylledeToiletter?: number | null;
+  /**
+   * EAID_580455CE_A3A3_4f20_805C_4A5CDCFF5221
+   */
+  enh066AntalBadeværelser?: number | null;
+  /**
+   * EAID_7C65C9B9_57C5_40d3_8E56_03BD0968FF7C
+   */
+  enh067Støjisolering?: number | null;
+  /**
+   * EAID_0D000A0C_C62D_4222_89D9_701C5C01622A
+   */
+  enh068FlexboligTilladelsesart?: string | null;
+  /**
+   * EAID_5DC383DA_255E_4f10_8B80_AF3336DE05FB
+   */
+  enh069FlexboligOphørsdato?: string | null;
+  /**
+   * EAID_A44137A2_5E9D_4aca_8A80_EBC80329F66D
+   */
+  enh070ÅbenAltanTagterrasseAreal?: number | null;
+  /**
+   * EAID_817DD6B4_1247_41f2_B4BC_0D0E8AE5F6B9
+   */
+  enh071AdresseFunktion?: string | null;
+  /**
+   * EAID_19EA8E50_0148_4c96_8994_BAE313DD60FE
+   */
+  enh101Gyldighedsdato?: string | null;
+  /**
+   * EAID_AEF160E3_D2F7_4d6e_BEE1_AC81F31142D3
+   */
+  enh102HerafAreal1?: number | null;
+  /**
+   * EAID_8A4DFAF0_1951_49d1_9F96_AAAB83560AEF
+   */
+  enh103HerafAreal2?: number | null;
+  /**
+   * EAID_C57AB150_EE77_4065_8911_AF1B955BF93B
+   */
+  enh104HerafAreal3?: number | null;
+  /**
+   * EAID_651F6757_9453_4132_9BB2_2080BC3594D5
+   */
+  enh105SupplerendeAnvendelseskode1?: string | null;
+  /**
+   * EAID_6ACC7F2F_2EC3_4c1e_B4F7_08893DD0B3BD
+   */
+  enh106SupplerendeAnvendelseskode2?: string | null;
+  /**
+   * EAID_74D2BAEB_8148_4319_9D39_238992392A45
+   */
+  enh107SupplerendeAnvendelseskode3?: string | null;
+  /**
+   * EAID_CCD38815_47CE_4d11_8D6A_B2F9A1824B8C
+   */
+  enh127FysiskArealTilBeboelse?: number | null;
+  /**
+   * EAID_CD596BE6_98A8_4ed4_A099_AB762D815BD9
+   */
+  enh128FysiskArealTilErhverv?: number | null;
+  /**
+   * EAID_9F6C43E3_7E32_42eb_BB05_027B0ACF1C10
+   */
+  enh500Notatlinjer?: string | null;
+  /**
+   * EAID_dstDC0604_11E5_4f1b_A9FA_E1921B9C2D05
+   */
+  etage?: string | null;
+  /**
+   * EAID_50066AF4_06D9_4f3a_BAC2_5F83ABF0E335
+   */
+  forretningshændelse?: string | null;
+  /**
+   * EAID_C91552B7_18C0_4e28_8B06_F60332A8776B
+   */
+  forretningsområde?: string | null;
+  /**
+   * EAID_E4132152_7923_4533_8529_5E8D8C937656
+   */
+  forretningsproces?: string | null;
+  /**
+   * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_9AB90AE0_9F85_4164_9B25_8EB2139D65A5
+   */
+  id_lokalId: string | null;
+  /**
+   * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_6CEB0356_5CBF_4159_B96B_A2489DD2DAC8
+   */
+  id_namespace?: string | null;
+  /**
+   * EAID_C95C717A_752C_4b17_B647_850738975CAC
+   */
+  kommunekode?: string | null;
+  /**
+   * EAID_dst633A5F_934F_447e_96DC_9C1C65E727A2
+   */
+  opgang?: string | null;
+  /**
+   * EAID_7A7F26D9_D5AE_4158_A7FF_3E6EF0B228D8
+   */
+  registreringFra?: string | null;
+  /**
+   * EAID_B3CCBA6B_52B2_4ddc_A751_ABEA39D6EE6B
+   */
+  registreringsaktør?: string | null;
+  /**
+   * EAID_E718B937_950A_4ea1_BFA0_2AE9DB8C43AD
+   */
+  registreringTil?: string | null;
+  /**
+   * EAID_6FAA8A1A_5BCF_4950_B220_F4AFCA7F35E3
+   */
+  status?: string | null;
+  /**
+   * EAID_A351A366_2F30_4c3f_B086_7FAA59339C5D
+   */
+  virkningFra?: string | null;
+  /**
+   * EAID_59A458B1_5844_4fe3_95B1_C38AD3AAFAF4
+   */
+  virkningsaktør?: string | null;
+  /**
+   * EAID_47628A51_6589_4f1e_A19F_E9943A136AEC
+   */
+  virkningTil?: string | null;
+  /**
+   * EAID_EA3E6D60_A489_416d_B88E_ADF2831667FA
+   */
+  fordelingsarealList?: {
+    /**
+     * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_9AB90AE0_9F85_4164_9B25_8EB2139D65A5
+     */
+    id_lokalId: string | null;
+    /**
+     * EAID_EA3E6D60_A489_416d_B88E_ADF2831667FA
+     */
+    fordelingsareal?: {
+      datafordelerOpdateringstid?: DatafordelerOpdateringstid1;
+      /**
+       * EAID_dst582669_503B_4e1e_A9F8_8651EEFF2799
+       */
+      bygning?: string | null;
+      /**
+       * EAID_1FA4BED5_6A55_4de2_B0C6_EBA1CBA77934
+       */
+      for002Fordelingsarealnummer?: number | null;
+      /**
+       * EAID_A769CEA2_37AA_40f3_A3C4_DD9B16403C1B
+       */
+      for003ArealTilFordeling?: number | null;
+      /**
+       * EAID_429C3249_3EE0_48e1_94C3_CD216571AE7A
+       */
+      for004FordelingsNøgle?: string | null;
+      /**
+       * EAID_5BBAA4C9_108D_4174_95D1_F5434033AAF2
+       */
+      for005Navn?: string | null;
+      /**
+       * EAID_0C38AFF6_BE4F_4924_AC4D_3D8E5FB6F729
+       */
+      for006Rest?: number | null;
+      /**
+       * EAID_F05AFC7F_D2DE_4f10_BAA4_35390CE924E0
+       */
+      for500Notatlinjer?: string | null;
+      /**
+       * EAID_50066AF4_06D9_4f3a_BAC2_5F83ABF0E335
+       */
+      forretningshændelse?: string | null;
+      /**
+       * EAID_C91552B7_18C0_4e28_8B06_F60332A8776B
+       */
+      forretningsområde?: string | null;
+      /**
+       * EAID_E4132152_7923_4533_8529_5E8D8C937656
+       */
+      forretningsproces?: string | null;
+      /**
+       * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_9AB90AE0_9F85_4164_9B25_8EB2139D65A5
+       */
+      id_lokalId?: string | null;
+      /**
+       * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_6CEB0356_5CBF_4159_B96B_A2489DD2DAC8
+       */
+      id_namespace?: string | null;
+      /**
+       * EAID_C95C717A_752C_4b17_B647_850738975CAC
+       */
+      kommunekode?: string | null;
+      /**
+       * EAID_7A7F26D9_D5AE_4158_A7FF_3E6EF0B228D8
+       */
+      registreringFra?: string | null;
+      /**
+       * EAID_B3CCBA6B_52B2_4ddc_A751_ABEA39D6EE6B
+       */
+      registreringsaktør?: string | null;
+      /**
+       * EAID_E718B937_950A_4ea1_BFA0_2AE9DB8C43AD
+       */
+      registreringTil?: string | null;
+      /**
+       * EAID_6FAA8A1A_5BCF_4950_B220_F4AFCA7F35E3
+       */
+      status?: string | null;
+      /**
+       * EAID_A351A366_2F30_4c3f_B086_7FAA59339C5D
+       */
+      virkningFra?: string | null;
+      /**
+       * EAID_59A458B1_5844_4fe3_95B1_C38AD3AAFAF4
+       */
+      virkningsaktør?: string | null;
+      /**
+       * EAID_47628A51_6589_4f1e_A19F_E9943A136AEC
+       */
+      virkningTil?: string | null;
+    };
+  }[];
+  /**
+   * EAID_0E5C322B_0AA8_4005_B17E_F0397230EE7D
+   */
+  ejerlejlighedList?: {
+    /**
+     * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_9AB90AE0_9F85_4164_9B25_8EB2139D65A5
+     */
+    id_lokalId: string | null;
+    /**
+     * EAID_0E5C322B_0AA8_4005_B17E_F0397230EE7D
+     */
+    ejerlejlighed?: {
+      datafordelerOpdateringstid?: DatafordelerOpdateringstid2;
+      /**
+       * EAID_DAEA052B_1881_496a_B35B_A7BE3B6B7586
+       */
+      bfeNummer?: number | null;
+      /**
+       * EAID_dst21CE45_7C49_4653_B220_8583C4941A98
+       */
+      bygningPåFremmedGrund?: string | null;
+      /**
+       * EAID_8B9A4700_4212_4dd6_95A3_A8FA2192AF4E
+       */
+      ejendommensEjerforholdskode?: string | null;
+      /**
+       * EAID_252012AE_E95C_4c9d_AB56_C5C763AC47DE
+       */
+      ejendomsnummer?: number | null;
+      /**
+       * EAID_9858CAD1_2FAA_4312_98DE_081D40EBC659
+       */
+      ejendomstype?: string | null;
+      /**
+       * EAID_dst549CF9_6E0F_428e_876D_664717D7B27A
+       */
+      ejerlejlighed?: string | null;
+      /**
+       * EAID_4B7C4E42_30D8_42cf_BF15_306AA4EA7F22
+       */
+      ejerlejlighedsnummer?: number | null;
+      /**
+       * EAID_50066AF4_06D9_4f3a_BAC2_5F83ABF0E335
+       */
+      forretningshændelse?: string | null;
+      /**
+       * EAID_C91552B7_18C0_4e28_8B06_F60332A8776B
+       */
+      forretningsområde?: string | null;
+      /**
+       * EAID_E4132152_7923_4533_8529_5E8D8C937656
+       */
+      forretningsproces?: string | null;
+      /**
+       * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_9AB90AE0_9F85_4164_9B25_8EB2139D65A5
+       */
+      id_lokalId?: string | null;
+      /**
+       * EAID_F8196C58_7115_48a6_9A1D_B49BC0F6289A.EAID_6CEB0356_5CBF_4159_B96B_A2489DD2DAC8
+       */
+      id_namespace?: string | null;
+      /**
+       * EAID_C95C717A_752C_4b17_B647_850738975CAC
+       */
+      kommunekode?: string | null;
+      /**
+       * EAID_7A7F26D9_D5AE_4158_A7FF_3E6EF0B228D8
+       */
+      registreringFra?: string | null;
+      /**
+       * EAID_B3CCBA6B_52B2_4ddc_A751_ABEA39D6EE6B
+       */
+      registreringsaktør?: string | null;
+      /**
+       * EAID_E718B937_950A_4ea1_BFA0_2AE9DB8C43AD
+       */
+      registreringTil?: string | null;
+      /**
+       * EAID_dstC65956_C726_418b_B8B5_F61903D9D8F8
+       */
+      samletFastEjendom?: string | null;
+      /**
+       * EAID_6FAA8A1A_5BCF_4950_B220_F4AFCA7F35E3
+       */
+      status?: string | null;
+      /**
+       * EAID_528134C9_8903_404d_968E_E13E09172A7B
+       */
+      tinglystAreal?: number | null;
+      /**
+       * EAID_A351A366_2F30_4c3f_B086_7FAA59339C5D
+       */
+      virkningFra?: string | null;
+      /**
+       * EAID_59A458B1_5844_4fe3_95B1_C38AD3AAFAF4
+       */
+      virkningsaktør?: string | null;
+      /**
+       * EAID_47628A51_6589_4f1e_A19F_E9943A136AEC
+       */
+      virkningTil?: string | null;
+      /**
+       * EAID_AEEAF515_0D26_4059_8DEC_DA61BD2A4EBD
+       */
+      vurderingsejendomsnummer?: number | null;
+    };
+  }[];
+}[];
