d78c03af1fdf96dc26e80f09ce7748762dec4926
Simon Saxtorph <simon@saxs.dk>
2025-05-20 11:13:18 +0200

    refactor: rename test suite to BBR and update test cases for consistency

--- clients/client-bbr/tests/e2e/bbr.e2e.spec.ts
+++ clients/client-bbr/tests/e2e/bbr.e2e.spec.ts
@@ -6,7 +6,7 @@ import { Livscyklus } from '../../src/livscyklus';
 const CERT_PATH = process.env.CERT_PATH as string;
 const CERT_PASSPHRASE = process.env.CERT_PASSPHRASE as string;
 
-describe('Ejerfortegnelsen', () => {
+describe('BBR', () => {
   let bbrWithAgent: BBR;
 
   beforeEach(() => {
@@ -22,20 +22,46 @@ describe('Ejerfortegnelsen', () => {
       Status: Livscyklus.Afsluttet,
     });
 
-    console.log(res);
-
     expect(res).toBeDefined();
     expect(res[0]?.sagsniveauList?.[0]?.sagsniveau).toBeDefined();
+    expect(res[0]?.sagsniveauList?.[0]).toHaveProperty('id_lokalid'); // This is a bug in the api response as the value should be id_lokalId
+    expect(res[0]?.sagsniveauList?.[0]).toHaveProperty('sagsniveau');
   });
 
   it('should get a bbr sag without depth', async () => {
     const res = await bbrWithAgent.BBRSag({
       Id: '9b1d2d6e-3e3e-4479-9869-64c72d666bf2',
       MedDybde: false,
-      Status: [Livscyklus.Afsluttet, Livscyklus.Gaeldende],
+      Status: [Livscyklus.Afsluttet, Livscyklus.Gældende],
     });
 
     expect(res).toBeDefined();
-    expect(res[0]?.sagsniveauList[0]?.id_lokalid).toBeDefined();
+    expect(res[0]?.sagsniveauList?.[0]).toBeDefined();
+    expect(res[0]?.sagsniveauList?.[0]).toHaveProperty('id_lokalid'); // This is a bug in the api response as the value should be id_lokalId
+    expect(res[0]?.sagsniveauList?.[0]).not.toHaveProperty('sagsniveau');
+  });
+
+  it('should get a bbr bygning with depth', async () => {
+    const res = await bbrWithAgent.Bygning({
+      Id: 'dbf0e197-e889-4dd8-b820-be5c0d243f6a',
+      Status: Livscyklus.Opført,
+    });
+
+    expect(res).toBeDefined();
+    expect(res[0]?.etageList?.[0]).toBeDefined();
+    expect(res[0]?.etageList?.[0]).toHaveProperty('id_lokalId');
+    expect(res[0]?.etageList?.[0]).toHaveProperty('etage');
+  });
+
+  it('should get a bbr bygning without depth', async () => {
+    const res = await bbrWithAgent.Bygning({
+      Id: 'dbf0e197-e889-4dd8-b820-be5c0d243f6a',
+      MedDybde: false,
+    });
+
+    expect(res).toBeDefined();
+    expect(res[0]?.etageList?.[0]).toBeDefined();
+    expect(res[0]?.etageList?.[0]).toHaveProperty('id_lokalId');
+    expect(res[0]?.etageList?.[0]).not.toHaveProperty('etage');
   });
 });
